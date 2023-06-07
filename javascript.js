2739e211731d0336e5d21eec3dd1307472e9b801
NinoTraverso <njt98git@proton.me>
2023-06-07 14:49:18 +0000

    first commit

---
+++ javascript.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("toDoListForm");
+
+/* ORA FACCIAMO IN MODO CHE LA PAGINA NON FA IL REFRESH */
+
+form.addEventListener("ADD_TASK", function (add) {
+  add.preventDefault();
+
+  /* ORA RECUPERIAMO I DATI DEI NOSTRI INPUT*/
+
+  const nameOfTask = document.getElementById("task");
+
+  /* ORA PRENDIAMO LE VALUE DI CIASCUNO E LI METTIAMO IN UN OBJECT DA USARE SUCESSIVAMENTE */
+
+  const newTask = nameOfTask.value;
+
+  const addedTask = {
+    addedTask: newTask,
+  };
+
+  /* ADESSO FACCIAMO IN MODO CHE IL TASK INSERITO VENGA MOSTRATO */
+
+  const button = document.getElementById("myButton");
+
+  // Add a class to the element
+  button.classList.add("myClass");
+
+  const addTask = document.createElement("div");
+  addTask.classList.add("ListOfTasks");
+
+  addTask.innerHTML = `
+  <input type="checkbox"><p> ${addedTask}</p> />
+
+  `;
+});
